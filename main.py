8c7403e976e8c507c15f9cf36fd80b481c03ef78
Astha27 <93661941+Rythmastha@users.noreply.github.com>
2023-09-23 19:15:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import pandas as pd
 import pickle
 
 app = Flask(__name__)
-data=pd.read_csv("Cleaned_data.csv")
+data = pd.read_csv("Cleaned_data.csv")
 
-pipe=pickle.load(open("final.pkl",'rb'))
+pipe = pickle.load(open("final.pkl", 'rb'))
 
 @app.route('/')
 def index():
-    locations=sorted(data['location'].unique())
-    return render_template('index.html',locations=locations)
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
 
-
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    location=request.form.get('location')
-    total_sqft=request.form.get('total_sqft')
-    bath=request.form.get('bath')
-    balcony=request.form.get('balcony')
-    bedrooms=request.form.get('bedrooms')
-    print(location,total_sqft,bath,balcony,bedrooms)
-    input=pd.DataFrame([[location,total_sqft,bath,balcony,bedrooms]],columns=['location','total_sqft','bath','balcony','bedrooms'])
-    prediction=pipe.predict(input)[0]
+    location = request.form.get('location')
+    total_sqft = request.form.get('total_sqft')
+    bath = request.form.get('bath')
+    balcony = request.form.get('balcony')
+    bedrooms = request.form.get('bedrooms')
+    print(location, total_sqft, bath, balcony, bedrooms)
+    input_data = pd.DataFrame([[location, total_sqft, bath, balcony, bedrooms]],
+                             columns=['location', 'total_sqft', 'bath', 'balcony', 'bedrooms'])
+    prediction = pipe.predict(input_data)[0]
 
     return str(prediction)
-if __name__=="__main__":
-    app.run(debug=True,port=5001)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
